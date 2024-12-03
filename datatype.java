0229506b770e26c510efb0ba57655ec069d9366f
navateja5994 <navatejar80@gmail.com>
2024-12-03 10:37:05 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ class dt{
+    public static void main(String args[])
+    {
+        int a=23;
+        char b='M';
+        float c=7.43f;
+        double d=123;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    
+    }
+}
