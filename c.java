028259ac91c3013f8104b5082d726cae8ea74f96
navateja5994 <navatejar80@gmail.com>
2024-12-04 15:29:52 +0530

    Add files via upload

---
+++ c.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class as{
+    public static void main(String args[]){
+        //accepting strings 
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String c=sc.next();
+        String b=String.format("the name of person is %s and age is"+c,a);
+        System.out.println(b);
+    }
+}
