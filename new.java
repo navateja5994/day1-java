5733071680a59a1550383614e9e610f8b1c57287
navateja5994 <navatejar80@gmail.com>
2024-12-04 15:30:22 +0530

    Add files via upload

---
+++ new.java
@@ -0,0 +1,10 @@
+import java.io.PrintWriter;
+public class p{
+    public static void main(String args[]){
+        PrintWriter a= new PrintWriter(System.out);
+        a.println("HELLO WORLD");
+        a.print("this is");
+        a.print("print statement");
+        a.close();
+
+    }
