923efde592bd98fd52b568216a5edf27116835a3
navateja5994 <navatejar80@gmail.com>
2024-12-04 15:29:37 +0530

    Add files via upload

---
+++ b.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class string{
+    public static void main(String args[]){
+        //accepting strings
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println("congractulations "+a+" you won 5 lakhs");
+    }
+}
